86feba32e0d25d7edf2dc5ff417603b5dff13d28
roshanak <roshankrishnaraj10@gmail.com>
2026-07-05 19:22:19 +0530

    wislist section added and profile section redesigned

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,13 +4,12 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext.jsx';
-import { WishlistProvider } from './context/wishlistContext.jsx'; // 1. Import it here
+import { WishlistProvider } from './context/wishlistContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartProvider>
-        {/* 2. Wrap it around your App */}
         <WishlistProvider>
           <App />
         </WishlistProvider>
