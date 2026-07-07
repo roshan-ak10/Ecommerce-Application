431b125f9bd63ccc248bed6cec731a263cceef72
roshanak <roshankrishnaraj10@gmail.com>
2026-07-07 13:58:51 +0530

    resolving error

--- Backend/server.js
+++ Backend/server.js
@@ -15,8 +15,10 @@ app.use(cookieParser());
 const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
 
 app.use(cors({
-  origin: allowedOrigin, 
-  credentials: true 
+  origin: ['https://ecommerce-application-mu-wheat.vercel.app' , 'http://localhost:5173'],
+  credentials: true ,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 const dbURI = process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce-dark-red';
